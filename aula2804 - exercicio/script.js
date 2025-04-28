3b611ee816a850dd5ddbbbcc128173b43ffdd933
Cyntia Sayuri <cy.sayuri@hotmail.com>
2025-04-28 20:42:13 -0300

    Personalização da camisa

--- aula2804 - exercicio/script.js
+++ aula2804 - exercicio/script.js
@@ -22,10 +22,15 @@ filter.forEach(elem => {
 
 
 const nome = document.querySelector('#nome')
-const btn = document.querySelector('#btnNome')
+const num = document.querySelector('#numero')
+const btn = document.querySelector('#btn')
 const nomeCamisa = document.querySelector('#camisa__nome')
+const numCamisa = document.querySelector('#camisa__numero')
 
 btn.addEventListener('click', () => {
     nomeUser = nome.value
     nomeCamisa.innerHTML = nomeUser
+    
+    numUser = num.value
+    numCamisa.innerHTML = numUser
 })
